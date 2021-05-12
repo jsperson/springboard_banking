e01eb4b12ae9ea3fccfed26cb9089ffa8350292b
Scott Person <jsperson@gmail.com>
2021-05-12 09:46:57 -0500

    Added comments and updated readme

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # Python OOP Mini-Project
 ## Banking System Case Study
  
+### Getting Started
+Execute: python.exe banking.py
 
-
-
+___
 Project format adapted from [The Hitchhiker's Guide to Python](https://docs.python-guide.org/writing/structure/).
--- banking.py
+++ banking.py
@@ -1,8 +1,7 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Date, Integer, String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy import create_engine, ForeignKey
 from sqlalchemy.orm import declarative_base, sessionmaker
-from sqlalchemy import Column, Integer, String
 
 import os
 import logging
@@ -10,10 +9,29 @@ import logging
 SQLAlchemy ORM code adapted from https://docs.sqlalchemy.org/en/14/orm/tutorial.html
 '''
 
-logging.basicConfig(format='%(asctime)s :: %(levelname)s :: %(funcName)s :: %(lineno)d \
-:: %(message)s', level=logging.INFO)
+# logging.basicConfig(format='%(asctime)s :: %(levelname)s :: %(funcName)s :: %(lineno)d \
+#:: %(message)s', level=logging.INFO, filename='banking.log')
+mylogs = logging.getLogger(__name__)
+mylogs.setLevel(logging.DEBUG)
+file = logging.FileHandler('banking.log')
+file.setLevel(logging.INFO)
+fileformat = logging.Formatter(
+    "%(asctime)s:%(levelname)s:%(message)s", datefmt="%H:%M:%S")
+file.setFormatter(fileformat)
+mylogs.addHandler(file)
+db_logger = logging.getLogger('sqlalchemy')
+db_logger.addHandler(file)
+db_logger.setLevel(logging.INFO)
+
 
 Base = declarative_base()
+base_file_path = os.path.dirname(os.path.abspath(__file__))
+helper_folder = 'banking'
+SQLITE_DB = 'banking.sqlite'
+full_db_name = f'{base_file_path}/{helper_folder}/{SQLITE_DB}'
+db_string = f'sqlite:///{full_db_name}'
+engine = create_engine(db_string, echo=False)
+Session = sessionmaker(bind=engine)
 
 
 class Customer(Base):
@@ -34,6 +52,15 @@ class Customer(Base):
             self.last_name, self.first_name, self.middle_name, self.street_address, self.city, self.zip, self.phone, self.email)
 
     def full_name(self):
+        '''      
+        Returns full name for the customer
+
+        Parameters: 
+            None
+
+        Returns:
+            First, Middle, Last names of the customer separated by spaces.
+        '''
         return f'{self.first_name} {self.middle_name} {self.last_name}'
 
 
@@ -50,45 +77,77 @@ class Account(Base):
             self.type, self.customer_id, self.balance)
 
     def format_balance(self):
+        '''      
+        Converts balance from an integer (*100) to a float. Balances are stored as integers.
+
+        Parameters: 
+            None
+
+        Returns:
+            Floating point representation of balance.
+        '''
         return float(self.balance)/100
 
     def withdrawal(self, amount):
+        '''      
+        Processes withdrawal operation from the account
+
+        Parameters: 
+            Amount to withdrawal
+
+        Returns:
+            Balance after withdrawal
+        '''
         if self.balance == None:
             self.balance = amount * -100
         else:
-            self.balance = self.balance - (amount * 100)
+            self.balance = self.balance - int(amount * 100)
         return self.format_balance()
 
     def deposit(self, amount):
+        '''      
+        Processes deposit operation into the account
+
+        Parameters: 
+            Amount to desposit
+
+        Returns:
+            Balance after deposit
+        '''
         if self.balance == None:
             self.balance = amount * 100
         else:
-            self.balance = self.balance + (amount * 100)
+            self.balance = self.balance + int(amount * 100)
         return self.format_balance()
 
 
 def init_data():
+    '''      
+    Initialize the database and insert some starter data.
+
+    Parameters: 
+        None
+
+    Returns:
+        None
+    '''
     if os.path.exists(full_db_name):
         os.unlink(full_db_name)
 
     Base.metadata.create_all(engine)
 
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    session.add_all([
-        Customer(last_name='Person', first_name='Jason', middle_name='Scott',
-                 street_address='105 N. Main', city='Longview', zip='73112', phone='555-123-4567', email='jason@spam.com'),
-        Customer(last_name='Person', first_name='Harper', middle_name='Jane',
-                 street_address='106 N. Main', city='Longview', zip='73112', phone='555-765-4321', email='harper@spam.com')])
-
-    session.commit()
-    session.close()
+    with Session() as session:
+        session.add_all([
+            Customer(last_name='Person', first_name='Jason', middle_name='Scott',
+                     street_address='105 N. Main', city='Longview', zip='73112', phone='555-123-4567', email='jason@spam.com'),
+            Customer(last_name='Person', first_name='Harper', middle_name='Jane',
+                     street_address='106 N. Main', city='Longview', zip='73112', phone='555-765-4321', email='harper@spam.com')])
+        session.commit()
 
 
 def print_menu():
     print(' ____________________________________')
-    print('| 0 - End Program                    |')
+    print('| 0 - End Session                    |')
     print('| 1 - List Customers                 |')
     print('| 2 - Set Current Customer           |')
     print('| 3 - Deposit                        |')
@@ -97,39 +156,29 @@ def print_menu():
 
 
 def main():
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    choice = ''
-    while choice != '0':
-        print_menu()
-        choice = input('Select option: ')
-        if choice == '1':
-            for instance in session.query(Customer).order_by(Customer.id):
-                print(f'{instance.id} {instance.full_name()}')
-        if choice == '2':
-            current_customer = input('Enter customer id: ')
-            # session.query(Account).filter_by(id=int(current_customer)).first()
-            current_account = Account(id=int(current_customer))
-        if choice == '3':
-            amount = input('Enter deposit amount: ')
-            new_balance = current_account.deposit(float(amount))
-            print(f'New Balance: {new_balance}')
-        if choice == '4':
-            amount = input('Enter withdrawal amount: ')
-            new_balance = current_account.withdrawal(float(amount))
-            print(f'New Balance: {new_balance}')
-
-    session.commit()
-    session.close()
+    with Session() as session:
+        choice = ''
+        while choice != '0':
+            print_menu()
+            choice = input('Select option: ')
+            if choice == '1':
+                for instance in session.query(Customer).order_by(Customer.id):
+                    print(f'{instance.id} {instance.full_name()}')
+            if choice == '2':
+                current_customer = input('Enter customer id: ')
+                # session.query(Account).filter_by(id=int(current_customer)).first()
+                current_account = Account(id=int(current_customer))
+            if choice == '3':
+                amount = input('Enter deposit amount: ')
+                new_balance = current_account.deposit(float(amount))
+                print(f'New Balance: {new_balance}')
+            if choice == '4':
+                amount = input('Enter withdrawal amount: ')
+                new_balance = current_account.withdrawal(float(amount))
+                print(f'New Balance: {new_balance}')
+        session.commit()
 
 
 if __name__ == '__main__':
-    base_file_path = os.path.dirname(os.path.abspath(__file__))
-    helper_folder = 'banking'
-    SQLITE_DB = 'banking.sqlite'
-    full_db_name = f'{base_file_path}/{helper_folder}/{SQLITE_DB}'
-    db_string = f'sqlite:///{full_db_name}'
-    engine = create_engine(db_string, echo=True)
-
     init_data()
     main()
